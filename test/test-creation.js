6ce590012d8dd617df04ecdd756e8c0dd785bcfa
Peter Sbarski <peter.sbarski@gmail.com>
2014-05-10 18:55:29 +1000

    tests

--- test/test-creation.js
+++ test/test-creation.js
@@ -20,6 +20,18 @@ describe('webapi generator', function () {
   it('creates expected files', function (done) {
     var expected = [
       // add files you expect to exist here.
+      'src/my-company.my-web-api.sln',
+      'src/my-company.my-web-api.web/my-company.my-web-api.web.csproj',
+      'src/my-company.my-web-api.web/Global.asax',
+      'src/my-company.my-web-api.web/Global.asax.cs',
+      'src/my-company.my-web-api.web/Web.config',
+      'src/my-company.my-web-api.web/Web.Debug.config',
+      'src/my-company.my-web-api.web/Web.Release.config',
+      'src/my-company.my-web-api.web/Properties/AssemblyInfo.cs',
+      'src/my-company.my-web-api.web/Controllers/ValuesController.cs',
+      'src/my-company.my-web-api.web/packages.config',
+      'package.json',
+      'bower.json',
       '.jshintrc',
       '.editorconfig'
     ];
